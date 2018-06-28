b8dec4d69fd2de3d00a94f0dedf91e2b88311d1e
marceauwr <marceauwangribour@hotmail.fr>
2018-06-28 18:44:22 +0200

    :speech_balloon: Adding text on new page

---
+++ views/pages/organe.php
@@ -0,0 +1,181 @@
+<div class="opacity"></div>
+<!-- HEADER -->
+<header class="header">
+    <div class="title">
+        <div class="title1">
+            <h1>Le</br>Départ</h1>
+        </div>
+
+        <div class="title2">
+            <h1>Arrivé</br>Pakistan</h1>
+        </div>
+
+        <div class="title3">
+            <h1>Hopital</h1>
+        </div>
+        <div class="line"></div>
+    </div>
+</header>
+
+<!-- MAIN -->
+<main class="main" data-page="<?php echo $page ?>">
+
+    <!-- Fullpage scrolling -->
+    <div id="fullpage">
+
+        <!-- section 0 -->
+        <div class="section active intro" id="section0">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <div class="scale">
+                    <img src="assets/images/backgrounds/home/bg-map.jpg" alt="">
+                    <div class="traficPoints">
+                        <!-- America trafic points -->
+                        <div></div>
+                        <div></div>
+
+                        <!-- Africa trafic points -->
+                        <div></div>
+                        <div></div>
+
+                        <!-- Europe trafic points -->
+                        <div></div>
+                        <div></div>
+                        <div></div>
+
+                        <!-- Asia trafic points -->
+                        <div></div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row center-align">
+                <h1 class="col s12">
+                </h1>
+            </div>
+        </div>
+
+        <!-- section 1 -->
+        <div class="section" id="section1">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <img src="assets/images/backgrounds/organe/bg-départ.jpg" alt="">
+            </div>
+            <div class="row">
+                <!-- <p class="col m5 push-m1 subheader">
+                    La pauvreté ! Un fléau dont les trafiquants profitent !
+                </p> -->
+                <p class="col s12 m8 offset-m2 center-align content">
+                Après des mois en liste d’attente pour une greffe de rein Alexandre décide de finalement partir au Pakistan. Il sait qu’il pourra acheter un rein là bas. C’est loin d’être éthique et c’est totalement illégal, mais : “J’ai vu trop de gens attendre leur greffe jusqu’à décéder, hors de question que je sois passif comme eux.”
+                </p>
+            </div>
+            <div class="opacityBack"></div>
+            <div class="information">
+                <a class="infoButton"><img src="assets/images/icons/agenda.png" alt=""></a>
+                <div class="infoSideBar valign-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <p class="col s12 infoContent">
+                                Le Pakistan est une plaque tournante du trafic d’organes. Au Pakistan le don d’organes est très strict pour 
+                                des questions religieuses on ne peut pas être donneur après la mort ce qui limite énormément le nombre de 
+                                donneurs possibles, de plus les dons ne sont possibles qu’entre membres proches de la même famille. 
+                                Cependant certains trafiquants ont très vite vu un marché prolifique. Une grande partie de la population 
+                                du pays est très pauvre et beaucoup de pakistanais sont endettés. Dans ce genre de cas la vente d’un organe 
+                                peut s’avérer être une option envisageable voir logique pour se sortir d’une mauvaise situation financière. 
+                                De plus ce trafic est géré par des associations criminelles organisées. Ces dernières endossent plusieurs rôles, 
+                                ils prêtent d’abord de grosses sommes d’argent, puis font chanter leurs “clients” et font pressions sur eux pour 
+                                qu’ils remboursent leurs dettes en donnant un organe qui sera ensuite revendu, en général un rein rapporte moins 
+                                de 1000 euros pour le vendeur. 
+                            </p>
+                        </div>
+                    </div>
+                    <a class="next"><img src="assets/images/icons/arrow.png" alt=""></a>
+                </div>
+                <div class="infoSideBarTwo valign-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <p class="col s12 infoContent">
+                                De plus dans la plupart des cas les donneurs ne sont pas assommés pour être posé 
+                                sur une table d’opération. Ils décident eux mêmes de vendre à cause de la pression des agents des mafias. 
+                                Ce sont ces derniers qui organisent ensuite les opérations la vente de l’organe au client pour des prix 
+                                variants en fonction de l’organe reçu allant de 25 000 à 100 000 euros. La police pakistanaise tente de 
+                                réprimer ce genre de pratiques mais les poursuites sont souvent sans suite comme les victimes des mafias 
+                                se prétendent “donneurs bénévoles” pour éviter le courroux des criminels. Les clients sont parfois de riches 
+                                palestiniens mais dans la plupart des cas ce sont des étrangers venus de pays comme le Royaume-Uni ou Israël 
+                                où le trafic existe dans une moindre mesure et à des prix plus élevés
+                            </p>
+                        </div>
+                    </div>
+                    <a class="previous"><img src="assets/images/icons/arrow.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+
+        <!-- section 2 -->
+        <div class="section history" id="section2">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <img src="assets/images/backgrounds/organe/bg-pakistan.jpg" alt="">
+            </div>
+            <div class="row">
+                <p class="col s12 m8 offset-m2 center-align content">
+                23 000 dollars, c’est le prix proposé par un “agent” pour un donneur et de l’aide afin d’obtenir un permis de greffe gouvernementale. Si les autorités veulent s’y opposer la plupart du temps le passage du vendeur devant un comité où il affirme qu’il souhaite donner un organe bénévolement suffit à bloquer les autorités. 
+                </p>
+            </div>
+            <div class="opacityBack"></div>
+            <div class="information">
+                <a class="infoButton"><img src="assets/images/icons/agenda.png" alt=""></a>
+                <div class="infoSideBar valign-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <p class="col s12 infoContent">
+                            Le pays est tellement impuissant face au trafic qu’un nouvel organisme a été chargé de lutter contre ce fléau, 
+                            l’Agence FIA. Ils sont la preuve au monde que les trafiquants ne resteront pas impunis à tout jamais cependant 
+                            leur pouvoir reste limité et certains hôpitaux privés sont tout simplement intouchables. De plus bien que des 
+                            actions isolés soient mené par la FIA pour sévir, le problème de fond n’est pas traité. Comme le dit Mumtaz 
+                            Ahmed, chef du service de néphrologie à l’hôpital public Benazir Bhutto à Rawalpindi, une ville de la périphérie 
+                            d’Islamabad : “Ce commerce illégal profite aux riches et à l’élite du pays”. La question de la corruption est 
+                            souvent soulevée par les experts qui jugent que si le gouvernement est réticent à faire appliquer la loi c’est 
+                            à cause des gains du trafic, on compte près de 1.2 milliards de dollars qui sont générés par le trafic d’organes 
+                            chaque année, pour certains lutter contre serait renoncer à des sommes élevées versées par les organisations 
+                            gérant l’ensemble de l’activité.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- section 3 -->
+        <div class="section" id="section3">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <img src="assets/images/backgrounds/organe/bg-hopital.jpg" alt="">
+            </div>
+            <div class="row">
+                <p class="col s12 m8 offset-m2 center-align content">
+                Le “donneur” est en fait une donneuse elle s’appelle Faiza et manque clairement d’argent. L’opération va être effectué puis Alexandre restera quelques jours pour se remettre et repartira en avion. 
+                </p>
+            </div>
+            <div class="opacityBack"></div>
+            <div class="information">
+                <a class="infoButton"><img src="assets/images/icons/agenda.png" alt=""></a>
+                <div class="infoSideBar valign-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <p class="col s12 infoContent">
+                                Alexandre rencontre la donneuse pour être rassuré sur l’origine de l’organe. Faiza avait déjà été briefée par “l’agent” pour paraître la plus en forme possible. Si besoin elle peut même être légèrement maquillée, tout est fait pour mettre le client en confiance, tant qu’il n’a pas encore payé. 
+                                Une fois la transaction effectuée l’apparence de Faiza compte tout de suite beaucoup moins, impossible pour Alexandre de récupérer le moindre dollar.
+                                Faiza elle explique qu’elle fait cette opération pour réunir assez d’argent afin de payer ses dettes. Et que, si elle avait eu un autre choix, elle n’aurait jamais vendu un rein pour quelques centaines de dollars.
+                                Dans la plupart des cas les opérations se déroulent dans des hôpitaux privés et les médecins donneurs et acheteurs ne sont pas inquiétés. Cependant parfois les opérations ont lieu dans dans des salles d’opérations improvisées et c’est que la plupart des trafiquants sont arrêtés par les services pakistanais. L’exemple le plus connu est celui de l’hôpital clandestin de Lahore où 16 suspects ou été arrêté les trafiquants ont dû payer une amende de 100 000 dollars. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div> <!-- fullpage end -->
