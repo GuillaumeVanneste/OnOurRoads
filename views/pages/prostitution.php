c17a1024a7339a9b12e1d39f63c290e5e2912cef
GuillaumeVanneste <guillaumevanneste@hotmail.fr>
2018-06-28 03:28:20 +0200

    :bento: adding text, videos, images and styles

---
+++ views/pages/prostitution.php
@@ -0,0 +1,172 @@
+<div class="opacity"></div>
+<!-- HEADER -->
+<header class="header">
+    <div class="title">
+        <div class="title1">
+            <h1>Pauvreté</br>Précarité</h1>
+        </div>
+
+        <div class="title2">
+            <h1>La Quête</br>D'Argent</h1>
+        </div>
+
+        <div class="title3">
+            <h1>Exportation</br>Menace</h1>
+        </div>
+        <div class="line"></div>
+    </div>
+</header>
+
+<!-- MAIN -->
+<main class="main">
+
+    <!-- Fullpage scrolling -->
+    <div id="fullpage">
+
+        <!-- section 0 -->
+        <div class="section active intro" id="section0">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <div class="scale">
+                    <img src="assets/images/backgrounds/home/bg-map.png" alt="">
+                    <div class="traficPoints">
+                        <!-- America trafic points -->
+                        <div></div>
+                        <div></div>
+
+                        <!-- Africa trafic points -->
+                        <div></div>
+                        <div></div>
+
+                        <!-- Europe trafic points -->
+                        <div></div>
+                        <div></div>
+                        <div></div>
+
+                        <!-- Asia trafic points -->
+                        <div></div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row center-align">
+                <h1 class="col s12">
+                </h1>
+            </div>
+        </div>
+
+        <!-- section 1 -->
+        <div class="section" id="section1">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <img src="assets/images/backgrounds/prostitution/bg-pauvrete.png" alt="">
+            </div>
+            <div class="row">
+                <p class="col m5 push-m1 subheader">
+                    La pauvreté ! Un fléau dont les trafiquants profitent !
+                </p>
+                <p class="col s12 m8 offset-m2 center-align content">
+                    Kebe, jeune nigériane de 13 ans : “Mon père est mort, j’ai 2 petites soeurs et ma mère ne gagne pas assez d’argent pour nous nourrir
+                    et payer le loyer. Je devais trouver un travail pour nous sortir de cette situation !”.
+                </p>
+            </div>
+            <div class="opacityBack"></div>
+            <div class="information">
+                <a class="infoButton"><img src="assets/images/icons/agenda.png" alt=""></a>
+                <div class="infoSideBar valign-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <p class="col s12 infoContent">
+                                L'extrême pauvreté, les troubles sociaux et économiques et la corruption sont les principales causes du problème international de l'esclavage sexuel et la traite humaine.
+                                Le Nigeria regroupe tout cela ainsi que des droits très limités pour les femmes et est le pays d’origine des victimes de traite à des fins d'exploitation sexuelle malgré le fait qu’il soit la deuxième puissance économique d’Afrique et dispose d’importante ressources pétrolières. Mais cette richesse a peu d'effet sur le niveau de vie de la population car plus de la moitié des Nigérians vit avec moins d'1,25 $ par jour. De ce fait, le pays est relativement pauvre et notamment à cause d’une forte corruption (le Nigeria est classé à la 130e place mondiale sur 180 dans le classement 200p de Transparency International).
+                                La forte présence de la corruption au sein du pays permet aux trafiquants d’échapper plus facilement à la justice. En effet, le gouvernement nigérian n’a prononcé que 30 condamnations pour traite en 2014, contre 42 en 2013, ce qui représente une diminution des condamnations.
+                                Quant à la pauvreté des nigérians, elle offre une perspective d'accroissement du trafic d’êtres humains, notamment à des fins de prostitutions, plus qu’important pour les trafiquants. En effet, les victimes sont souvent d'origine rurale et se retrouvent dans des maisons closes dans les grandes villes, en particulier à Lagos et dans l'Etat d'Edo. C’est donc vers ces personnes vulnérable et manipulable que les trafiquants se tournent.
+                                Au Nigeria, les enfants, essentiellement les jeunes filles, sont les cibles principales des trafiquants. Plus de 50 000 jeunes femmes entre 9 et 17 ans sont victimes de trafic à des fins de prostitution. Mais il existe aussi une population masculine prostituée estimée à 865 à Lagos, 642 à Kano et 358 à Port Harcourt.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- section 2 -->
+        <div class="section history" id="section2">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <img src="assets/images/backgrounds/prostitution/bg-argent.png" alt="">
+            </div>
+            <div class="row">
+                <p class="col s12 m5 offset-m1 content">
+                    La traite des êtres humains remonte déjà à l’antiquité au sein de
+                    l’empire romain où les esclaves étaient monnaies courantes, un
+                    véritable bien de luxe aux multiples fonctions. Aujourd’hui la traite des
+                    humains est le troisième marché illégal le plus lucratif.
+                </p>
+            </div>
+            <div class="opacityBack"></div>
+            <div class="information">
+                <a class="infoButton"><img src="assets/images/icons/agenda.png" alt=""></a>
+                <div class="infoSideBar valign-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <h6 class="col s12 infoTitle">Qu’est-ce que c’est ?</h6>
+                        </div>
+                        <div class="row">
+                            <p class="col s12 infoContent">
+                                L’expression « trafic illicite de migrants » désigne le fait d’assurer, afin d’en tirer, directement ou
+                                indirectement, un avantage financier ou un autre avantage matériel, l’entrée illégale dans un État Partie
+                                d’une personne qui n’est ni un ressortissant ni un résident permanent de cet État.</br>
+                                À l’origine la traite d’humains était un commerce parfaitement légal et accepté de tous. Petit à petit
+                                le temps à vu naître des lois interdisant ces pratiques qui ont parfois mené à des guerres.
+                                Cependant malgré toutes les lois existantes les traites existent toujours bien qu’elles aient pris de
+                                nouvelle formes. On peut même affirmer que chaque année le nombre de victimes de ces commerces illégaux
+                                augmente légèrement.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- section 3 -->
+        <div class="section" id="section3">
+            <div class="background js-cursor-parallax center-align" data-amplitude="1.8" >
+                <div class="opacity"></div>
+                <img src="assets/images/backgrounds/prostitution/bg-menace.png" alt="">
+            </div>
+            <div class="row">
+                <p class="col s12 m5 offset-m1 content">
+                    La traite des êtres humains remonte déjà à l’antiquité au sein de
+                    l’empire romain où les esclaves étaient monnaies courantes, un
+                    véritable bien de luxe aux multiples fonctions. Aujourd’hui la traite des
+                    humains est le troisième marché illégal le plus lucratif.
+                </p>
+            </div>
+            <div class="opacityBack"></div>
+            <div class="information">
+                <a class="infoButton"><img src="assets/images/icons/agenda.png" alt=""></a>
+                <div class="infoSideBar valign-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <h6 class="col s12 infoTitle">Qu’est-ce que c’est ?</h6>
+                        </div>
+                        <div class="row">
+                            <p class="col s12 infoContent">
+                                L’expression « trafic illicite de migrants » désigne le fait d’assurer, afin d’en tirer, directement ou
+                                indirectement, un avantage financier ou un autre avantage matériel, l’entrée illégale dans un État Partie
+                                d’une personne qui n’est ni un ressortissant ni un résident permanent de cet État.</br>
+                                À l’origine la traite d’humains était un commerce parfaitement légal et accepté de tous. Petit à petit
+                                le temps à vu naître des lois interdisant ces pratiques qui ont parfois mené à des guerres.
+                                Cependant malgré toutes les lois existantes les traites existent toujours bien qu’elles aient pris de
+                                nouvelle formes. On peut même affirmer que chaque année le nombre de victimes de ces commerces illégaux
+                                augmente légèrement.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div> <!-- fullpage end -->
